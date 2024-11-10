58ba22e3c96dc76f0f8bcb20ac46fbc231d12ba8
Tiemi <jellyuura@gmail.com>
2024-11-10 16:09:35 -0300

    Add recursos ao app

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,8 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Merriweather-Bold.ttf", "MerriweatherBold");
+                    fonts.AddFont("Merriweather-Regular.ttf", "MerriweatherRegular");
                 });
 
 #if DEBUG
