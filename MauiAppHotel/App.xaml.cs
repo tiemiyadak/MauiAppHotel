2de3427c1d0a92304c9279ed9c22bb6fe5c071fb
Tiemi <jellyuura@gmail.com>
2024-11-10 18:09:09 -0300

    Add atualizações

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,13 +1,44 @@
 ﻿
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public  List <Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 80.0,
+                ValorDiariaCrianca = 40.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50.0,
+                ValorDiariaCrianca = 25.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiariaAdulto = 30.0,
+                ValorDiariaCrianca = 15.0
+            }
+
+        };
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new Views.Sobre());
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
--- MauiAppHotel/Views/HospedagemContratada.xaml.cs
+++ MauiAppHotel/Views/HospedagemContratada.xaml.cs
@@ -6,4 +6,15 @@ public partial class HospedagemContratada : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PopAsync();
+		} catch (Exception ex)
+		{
+			DisplayAlert("Ops!", ex.Message, "OK");
+		}
+    }
 }
